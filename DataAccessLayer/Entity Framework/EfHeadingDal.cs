2ca44d64ac863fdabfe5229b08c08c585ea1b28e
FATMATURKYILMAZ <fatma_turkyilmaz>
2025-07-12 23:22:13 +0300

    Headings verilerini admin layout üzerinden UI katmanına entegre ederek verileri sayfada gösterdik. Kategori ve headings ilişkisi sayesinde Heading in bağlı olduğu kategori adıda tabloda görünüyor.

--- DataAccessLayer/Entity Framework/EfHeadingDal.cs
+++ DataAccessLayer/Entity Framework/EfHeadingDal.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity; // Include için gerekli!
 
 namespace DataAccessLayer.Entity_Framework
 {
-    internal class EfHeadingDal
+    public class EfHeadingDal : GenericRepository<Heading>, IHeadingDal
     {
+        public List<Heading> GetListWithCategory()
+        {
+            using (var context = new Context())
+            {
+                return context.Headings.Include(x => x.Category).ToList();
+            }
+        }
     }
 }
